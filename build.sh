543c9cf44174233698c166a324f888e327ec245a
Alexandre Leite <acl1970@gmail.com>
2026-03-07 15:50:24 -0300

    feat: adiciona lógica para obter nome do arquivo zip a partir da configuração do npm

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 npm run build
+
+# Pegamos o valor da config diretamente do npm e guardamos numa variável local do script
+ZIP_NAME=$(npm pkg get config.zipName | tr -d '"')
+
+# Agora sim o comando tem o arquivo correto
+sha256sum "$ZIP_NAME"
