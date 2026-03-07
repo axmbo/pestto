71c8abd4e932beef985f464942391c3d7f06f54b
Alexandre Leite <acl1970@gmail.com>
2026-03-07 14:30:04 -0300

    feat: adiciona script para sincronizar versão do package.json no manifest.json

---
+++ sync-version.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const packageJson = require('./package.json');
+const manifestJson = require('./manifest.json');
+
+// Copia a versão do package.json (o Chefe) para o manifest.json
+manifestJson.version = packageJson.version;
+
+// Salva o manifest.json atualizado
+fs.writeFileSync('./manifest.json', JSON.stringify(manifestJson, null, 2));
+
+console.log(`✅ manifest.json sincronizado para a versão ${packageJson.version}`);
