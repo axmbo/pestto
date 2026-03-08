7c91b9839b958bbc23c0c931eefd640df4d2da86
Alexandre Leite <acl1970@gmail.com>
2026-03-08 14:31:49 -0300

    feat: adiciona geração de informações de build no script de build

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-npm run build
+# Pega as infos: Versão, Data/Hora e os 7 primeiros caracteres do Hash do Git
+VERSION=$(npm pkg get version | tr -d '"')
+DATETIME=$(date +'%Y-%m-%d %H:%M')
+GIT_HASH=$(git rev-parse --short HEAD)
+
+INFO="Pestto v$VERSION ($DATETIME) [$GIT_HASH]"
+
+# Cria o arquivo version.js do zero
+echo "const BUILD_INFO = '$INFO';" > version.js
+
+# Gera o zip incluindo o novo arquivo
+npm run build # O seu script de build no package.json deve incluir o version.js agora
 
 # Pegamos o valor da config diretamente do npm e guardamos numa variável local do script
 ZIP_NAME=$(npm pkg get config.zipName | tr -d '"')
